37628ebdce12512905a7122fa6acc17642786ee2
sanika patil <120210186+patilsanika199@users.noreply.github.com>
2024-10-11 10:27:50 +0530

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+printf("\n\n This C Program is Generated On GitHub Platform\n\n");
+printf("\n\n This C Program Executed Using Google App Engine \n\n");
+return 0;
+
+}
