7fd4205b7059ed501611928663cfffa828d4699f
sanika patil <120210186+patilsanika199@users.noreply.github.com>
2024-10-11 10:35:09 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo{
+public static void main(String args[]){
+System.out.println("This java Program is Generated On GitHub Platform");
+System.out.println("This java Program is Executed Using Google AppEngine");
+
+}
+}
